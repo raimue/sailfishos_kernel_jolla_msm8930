176646906ca878e8a86082654fb85987476fd9d1
Rainer Müller <raimue@codingfarm.de>
2014-01-05 16:54:21 +0100

    Make boot image script compatible with Android
    The create-boot-img.sh was not compatible with the standard mkbootimg
    from the Android platform tools which uses slightly different command
    line arguments.

--- scripts/create-boot-img.sh
+++ scripts/create-boot-img.sh
@@ -14,7 +14,7 @@ echo "Creating $BOOTIMG.."
 
 KERNEL=$(find `pwd`/arch/arm/boot/zImage)
 
-${BINARY_PATH}mkbootimg --kernel $KERNEL --ramdisk ${RAMDISKFILE}.gz --cmdline "init=/sbin/preinit root=/dev/mmcblk0p28 rootfstype=btrfs rootflags=recovery noinitrd androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3 maxcpus=2" --base 0x80200000 --offset 0x02000000 --pagesize 2048 -o $BOOTIMG
+${BINARY_PATH}mkbootimg --kernel $KERNEL --ramdisk ${RAMDISKFILE}.gz --cmdline "init=/sbin/preinit root=/dev/mmcblk0p28 rootfstype=btrfs rootflags=recovery noinitrd androidboot.hardware=qcom user_debug=31 ehci-hcd.park=3 maxcpus=2" --base 0x00000000 --pagesize 2048 --kernel_offset 0x80208000 --ramdisk_offset 0x82200000 --second_offset 0x81100000 --tags_offset 0x80200100 --board '' -o $BOOTIMG
 
 echo "Cleaning up.."
 rm $RAMDISKFILE*
